390c6959289d23494afa54df82c3e95f28813dae
NahuelUboldi <nahueluboldi@gmail.com>
2022-11-22 16:14:37 -0300

    trying to fetch api: cors problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   fetch('https://pnkwnu.deta.dev/prode/standings', {
+  //     mode: 'no-cors', // no-cors, *cors, same-origin
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const fetchPositions = () => {
+    const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+
+    const yourUrl = 'pnkwnu.deta.dev/prode/standings';
+
+    fetch(corsAnywhere + yourUrl, {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchPositions();
+  }, []);
+
+  return <div className='App'></div>;
 }
 
-export default App
+export default App;
